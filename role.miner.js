7291ec2c7e836e135ed158f04a8fc7a58f02df0a
Jiří Hofman <hofman@ders.cz>
2017-07-06 16:02:06 +0200

    eslinting

--- role.miner.js
+++ role.miner.js
@@ -1,25 +1,25 @@
 module.exports = {
     // a function to run the logic for this role
-    run: function (creep) {
+  run: function (creep) {
         // get source
-        let source = Game.getObjectById(creep.memory.sourceId);
+    let source = Game.getObjectById(creep.memory.sourceId);
         // find container next to source
-        let container = source.pos.findInRange(FIND_STRUCTURES, 1, {
-            filter: s => s.structureType == STRUCTURE_CONTAINER
-        })[0];
+    let container = source.pos.findInRange(FIND_STRUCTURES, 1, {
+      filter: s => s.structureType === STRUCTURE_CONTAINER
+    })[0];
 
         // if creep is on top of the container
-        if (creep.pos.isEqualTo(container.pos)) {
+    if (creep.pos.isEqualTo(container.pos)) {
             // harvest source
-            creep.harvest(source);
-            if (source.energy === 0){
-                creep.say(source.ticksToRegeneration);
-            }
-        }
-        // if creep is not on top of the container
-        else {
-            // move towards it
-            creep.moveTo(container);
-        }
+      creep.harvest(source);
+      if (source.energy === 0){
+        creep.say(source.ticksToRegeneration);
+      }
     }
+        // if creep is not on top of the container
+    else {
+            // move towards it
+      creep.moveTo(container);
+    }
+  }
 };
