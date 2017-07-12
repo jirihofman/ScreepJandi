37772ecabf72a334d0206b7d82b861b2fe8b4b88
Jiří Hofman <hofman@ders.cz>
2017-07-12 12:52:45 +0200

    eslinting builder add LAB as second priority construction site

--- role.builder.js
+++ role.builder.js
@@ -2,96 +2,96 @@ var roleUpgrader = require('role.upgrader');
 
 module.exports = {
     // a function to run the logic for this role
-    run: function (creep) {
+  run: function (creep) {
         // if target is defined and creep is not in target room
-        if (creep.memory.target != undefined && creep.room.name != creep.memory.target) {
+    if (creep.memory.target && creep.room.name !==creep.memory.target) {
             // find exit to target room
-            var exit = creep.room.findExitTo(creep.memory.target);
+      var exit = creep.room.findExitTo(creep.memory.target);
             // move to exit
-            creep.moveTo(creep.pos.findClosestByRange(exit));
-            creep.say('B->exit')
+      creep.moveTo(creep.pos.findClosestByRange(exit));
+      creep.say('B->exit');
             // return the function to not do anything else
-            return;
-        }
+      return;
+    }
 
         // if creep is trying to complete a constructionSite but has no energy left
-        if (creep.memory.working == true && creep.carry.energy == 0) {
+    if (creep.memory.working === true && creep.carry.energy === 0) {
             // switch state
-            creep.memory.working = false;
-            creep.memory.maxed   = false;
-        }
+      creep.memory.working = false;
+      creep.memory.maxed   = false;
+    }
         // if creep is harvesting energy but is full
-        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+    else if (creep.memory.working === false && creep.carry.energy === creep.carryCapacity) {
             // switch state
-            creep.memory.working = true;
-        }
+      creep.memory.working = true;
+    }
 
-        if (creep.memory.maxed === true){
+    if (creep.memory.maxed === true){
             // sam reknu, ze ma jit makat s tim objemem co ma v sobe
-            creep.memory.working = true;
-        }
+      creep.memory.working = true;
+    }
 
         // if creep is supposed to complete a constructionSite
-        if (creep.memory.working == true) {
+    if (creep.memory.working === true) {
 
             // find closest constructionSite. Non-roads, non-labs first
-            var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES, {
-                filter: s => (s.structureType !== STRUCTURE_ROAD) && (s.structureType !== STRUCTURE_LAB)
-            }) || creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+      var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES, {
+        filter: s => (s.structureType !== STRUCTURE_ROAD) && (s.structureType !== STRUCTURE_LAB) && (s.structureType !== STRUCTURE_TERMINAL)
+      }) || creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
             // if one is found
-            if (constructionSite != undefined) {
+      if (constructionSite) {
                 // try to build, if the constructionSite is not in range
-                if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
+        if (creep.build(constructionSite) === ERR_NOT_IN_RANGE) {
                     // move towards the constructionSite
-                    creep.moveTo(constructionSite);
-                } else {
-                    creep.say("Building ...")
-                }
-            }
-            // if no constructionSite is found
-            else {
-                // go upgrading the controller
-                roleUpgrader.run(creep);
-                creep.say("B->U")
-            }
+          creep.moveTo(constructionSite);
+        } else {
+          creep.say('Building ...');
         }
+      }
+            // if no constructionSite is found
+      else {
+                // go upgrading the controller
+        roleUpgrader.run(creep);
+        creep.say('B->U');
+      }
+    }
         // if creep is supposed to get energy
-        else {
+    else {
             // find closest container
-            let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: s => (
-                  (s.structureType == STRUCTURE_CONTAINER || s.structureType == STRUCTURE_STORAGE) && s.store[RESOURCE_ENERGY] > 50 /* Nesockuju u minera, ktery to tam sype po 10 */
+      let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: s => (
+                  (s.structureType === STRUCTURE_CONTAINER || s.structureType === STRUCTURE_STORAGE) && s.store[RESOURCE_ENERGY] > 50 /* Nesockuju u minera, ktery to tam sype po 10 */
                 )
               || _.some(Game.flags, c => c.color === COLOR_YELLOW && c.secondaryColor === COLOR_YELLOW && c.pos.isEqualTo(s.pos) && s.energy > creep.carryCapacity)
-            });
+      });
 
             // if no found, try extension when emergency
-            if (container == undefined) {
-                container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+      if (!container) {
+        container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                   // TODO docs for creep.memory.ext
-                    filter: s => s.structureType == STRUCTURE_EXTENSION && creep.memory.ext && s.energy > 0
-                });
-            }
+          filter: s => s.structureType === STRUCTURE_EXTENSION && creep.memory.ext && s.energy > 0
+        });
+      }
 
             // if one was found
-            if (container != undefined) {
-                if (container.structureType === STRUCTURE_EXTENSION)
-                    creep.say('B<-ext')
+      if (container) {
+        if (container.structureType === STRUCTURE_EXTENSION)
+          {creep.say('B<-ext');}
                 // try to withdraw energy, if the container is not in range
-                if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        if (creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                     // move towards it
-                    creep.moveTo(container);
-                }
-            }
-            else {
-                // find closest source
-                var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-                // try to harvest energy, if the source is not in range
-                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                    // move towards it
-                    creep.moveTo(source);
-                }
-            }
+          creep.moveTo(container);
         }
+      }
+      else {
+                // find closest source
+        var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+                // try to harvest energy, if the source is not in range
+        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+                    // move towards it
+          creep.moveTo(source);
+        }
+      }
     }
+  }
 };
