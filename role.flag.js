a61d7a4c6be38a99b6199d9580a77718f52d5c48
Jiří Hofman <hofman@ders.cz>
2017-08-23 08:17:23 +0200

    Flag building enhancement  - green - tower  - purple - link, specification whether source/target according to flagname  - red - container

--- role.flag.js
+++ role.flag.js
@@ -13,6 +13,29 @@ module.exports = {
         }
         break;
       }
+      case COLOR_RED: {
+        if (flag.room.controller.level < 3){
+            break;
+        }
+        let b = flag.room.createConstructionSite(flag.pos, STRUCTURE_CONTAINER);
+        if (b === 0){
+          console.log('createConstructionSite STRUCTURE_CONTAINER: ', flag.pos.x, flag.pos.y, flag.room);
+          flag.remove(); // remove the flag when the constructionSite is created
+        }
+        break;
+      }
+      case COLOR_PURPLE: {
+          if (/^s/.test(flag.name)){
+              //console.log(flag, " is a source")
+          }
+        let b = flag.room.createConstructionSite(flag.pos, STRUCTURE_LINK);
+        if (b === 0){
+          console.log('createConstructionSite STRUCTURE_LINK: ', flag.pos.x, flag.pos.y, flag.room);
+          // TODO: Nastavit jestli je to link na prijimani nebo psilani
+          flag.remove(); // remove the flag when the constructionSite is created
+        }
+        break;
+      }
       case COLOR_WHITE: {
         let b = flag.room.createConstructionSite(flag.pos, STRUCTURE_STORAGE);
         if (b === 0){
@@ -30,6 +53,14 @@ module.exports = {
           flag.remove(); // remove the flag when the constructionSite is created
         } else {
           console.log('Flag constructionSite error: ', b, flag.room, JSON.stringify(flag.pos));
+        }
+        break;
+      }
+      case COLOR_GREEN: {
+        let b = flag.room.createConstructionSite(flag.pos, STRUCTURE_TOWER);
+        if (b === 0){
+          console.log('createConstructionSite STRUCTURE_TOWER: ', flag.pos.x, flag.pos.y, flag.room);
+          flag.remove(); // remove the flag when the constructionSite is created
         }
         break;
       }
