889a9a9454b4728358c035da268acbbae33911cb
Jiří Hofman <hofman@ders.cz>
2017-08-23 08:18:41 +0200

    mineral lorry improvements

--- role.lorry_mineral.js
+++ role.lorry_mineral.js
@@ -2,7 +2,7 @@ module.exports = {
   // a function to run the logic for this role
   run: function (creep) {
     let l_creep_carry = _.sum(creep.carry);
-    creep.say('[M]');
+    creep.say('[M]' + creep.memory._task.mineral_type);
     // if creep is bringing energy to a structure but has no energy left
     if (creep.memory.working === true && l_creep_carry === 0) {
       // switch state
@@ -10,7 +10,7 @@ module.exports = {
       creep.memory.maxed   = false;
     }
     // if creep is harvesting energy but is full
-    else if (creep.memory.working === false && (l_creep_carry === creep.carryCapacity || creep.memory.maxed)) {
+    else if (creep.memory.working === false && (l_creep_carry > 5 || creep.memory.maxed)) {
       // switch state
       creep.memory.working = true;
     }
@@ -47,7 +47,7 @@ module.exports = {
         } else if (w === 0) {
           creep.say('👍');
         } else {
-          console.log('Error: ', w);
+          console.log('Error mineral lorry: ', w);
         }
       }
     }
