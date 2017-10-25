fd87d097c92a936e9d604ea13946d97b641e1bbe
Jiří Hofman <hofman@ders.cz>
2017-10-25 22:48:21 +0200

    reset number of LDHs in every room, then count them

--- room.planner.ldh.js
+++ room.planner.ldh.js
@@ -7,20 +7,20 @@ module.exports = {
     let l_ldhs = _.filter(Game.creeps, a=>a.memory && a.memory.role === 'longDistanceHarvester' && a.memory.home === l_name);
     let l_ldhs_pocet = _.size(l_ldhs);
     console.log('room.planner.ldhs.js start', r, l_ldhs_pocet, l_name);
+    if (Memory.rooms[r.name].ldh){
+      // set all numbers to zero
+      _.forEach(Memory.rooms[r.name].ldh, (v, k) => {
+        Memory.rooms[r.name].ldh[k].n = 0;
+        Memory.rooms[r.name].ldh[k].spawning = 0;
+      });
+    }
     this.set_ldhs(r, l_ldhs);
   },
 
   /* sets current number of LDharvesters into Memory for each room */
   set_ldhs: function(r, ldhs){
-    // set all numbers to zero
-    _.forEach(ldhs, (v) => {
-      if (Memory.rooms[r.name].ldh && Memory.rooms[r.name].ldh[v.memory.target]){
-        Memory.rooms[r.name].ldh[v.memory.target].n = 0;
-        Memory.rooms[r.name].ldh[v.memory.target].spawning = 0;
-      }
-    });
     //console.log('set_labs start', r, labs);
-    _.forEach(ldhs, (v, k) => {
+    _.forEach(ldhs, (v) => {
       //console.log('--set_ldhs', r.name, k, v, v.name, v.memory.target);
       if (!Memory.rooms[r.name].ldh){
         Memory.rooms[r.name].ldh = {};
